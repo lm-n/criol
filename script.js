a709c42cb77384e15fdf3fdd21b6af3e3b803d60
Luis Morales-Navarro <lmn297@nyu.edu>
2017-08-07 12:24:10 -0600

    added autofocus

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-var numRequests;
-var apiTermResponses = [];
+
+
 var finaltextIsReady = false;
 var loadtext = false;
-var thetlontextcat;
 var randomnumber;
 var thetlontext = "Their language and those things derived from their language—religion, literature, metaphysics—presuppose idealism. For the people of Tlön,the world is not an amalgam of objects in space; it is a heterogeneous series of independent acts the world is successive, temporal, but not spatial. There are no nouns in the conjectural Ursprache of Tlön,from which its present-day languages and dialects derive: there are impersonal verbs, modified by mono-syllabic suffixes (or prefixes) functioning as adverbs.";
-var theDT;
 var finaltext = "Click on the buttons to see examples of Tlönized English and learn more about the world of Tlön";
-var currentsentence;
-var ritatestPOS;
-var noundefinition;
-var theRitaTextArray = [];
-var dtplusPOSArray = [];
-var defs = [];
-var newTextArrays = [];
-var termAndDef = [];
-var nouns = [];
 var thetlontextor = "";
 
 function replacef(){
@@ -50,13 +39,12 @@ function myFunction() {
 }
 
 function resetGlobals(){
-	theRitaTextArray = [];
-	dtplusPOSArray = [];
-	defs = [];
-	newTextArrays = [];
-	termAndDef = [];
-	nouns = [];
-	apiTermResponses = [];
 	finaltext = 'Traduciendo...';
 	randomnumber = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
 }
+
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        myFunction();
+    }
+});
